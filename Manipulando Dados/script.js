2f5f67229622e45ca2708d83fad09fb41f7401cc
Ozeias Santos <ozeiasbss@gmail.com>
2022-09-29 14:27:55 -0300

    studing data

---
+++ Manipulando Dados/script.js
@@ -0,0 +1,8 @@
+// Manipulando Strings e Números
+
+// Contar quantos carateres tem um palavra e quantos dígitos tem um número.
+
+let word = "paralelepipedo"
+console.log(word.length)
+let number = 12345
+console.log(String(number).length)
---
+++ Manipulando Dados/script_1prototype.js
@@ -0,0 +1,8 @@
+/*
+    prototype
+    
+    *prototype-based language
+    *prototype chain
+    *__proto__
+ */
+    "ozeias".length
